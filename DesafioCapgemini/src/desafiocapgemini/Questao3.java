4b40fcc3cce2ed4c0b5beed9c35df6d4a970d0c8
Bianca Peixoto <biancap519@gmail.com>
2022-02-17 23:45:56 -0200

    final commit

--- DesafioCapgemini/src/desafiocapgemini/Questao3.java
+++ DesafioCapgemini/src/desafiocapgemini/Questao3.java
@@ -14,43 +14,42 @@ public class Questao3 {
         Arrays.sort(vetor1);                //ordenar os caracteres dos arrays
         Arrays.sort(vetor2);
 
-        if (s1.length() == s2.length()) {
+        if (s1.length() == s2.length()) {        //checar se substring são iguais em tamanho
             for (int i = 0; i < s1.length(); i++) {
-                if (vetor1[i] != vetor2[i]) // checar se as substrings sao diferentes
-                {
-                    return false;           //caso sejam diferentes, retornar falso
+                if (vetor1[i] != vetor2[i]) {
+                    return false;
                 }
             }
             return true;
         } else {
-            return false;
+            return false;            //caso sejam diferentes, retornar falso
         }
     }
 
     public static int anagrama(String n) {
         int cont = 0;
-        for (int i = 1; i < n.length(); i++) {
-            List<String> lista = new ArrayList<>();  //lista para armazenar as substrings
-            for (int j = 0; j <= n.length(); j++) {
-                if (i + j <= n.length()) {
-                    lista.add(n.substring(j, i + j));  // armazenando as substrings na lista
-                }
+        List<String> lista = new ArrayList<>(); //lista para armazenar as substrings
+        for (int i = 0; i < n.length(); i++) {
+            for (int j = i + 1; j <= n.length(); j++) {
+                lista.add(n.substring(i, j));  // armazenando as substrings na lista
             }
-            for (int k = 0; k < lista.size(); k++) {
-                for (int m = k + 1; m < lista.size(); m++) {
-                    if (verificarAnagrama(lista.get(k), lista.get(m))) {  //verificar se substrings são anagramas
-                        cont++;                 //se sim, acrescenta +1 ao contador
-                    }
+        }
+        for (int k = 0; k < lista.size(); k++) {
+            for (int m = k + 1; m < lista.size(); m++) {
+                if (verificarAnagrama(lista.get(k), lista.get(m))) {  //verificar se substrings são anagramas
+                    cont++;                             //se sim, acrescenta +1 ao contador
                 }
             }
         }
+
         return cont;
     }
 
     public static void main(String[] args) {
         Scanner tcl = new Scanner(System.in);
         System.out.println("Digite uma palavra: "); //entrar com a palavra
-        String n = tcl.next();                      // palavra armazenada na variavel n
+        String n = tcl.next();          // palavra armazenada na variavel n
+        n = n.replace(" ", "").toLowerCase();
         System.out.println(anagrama(n));
         tcl.close();
     }
